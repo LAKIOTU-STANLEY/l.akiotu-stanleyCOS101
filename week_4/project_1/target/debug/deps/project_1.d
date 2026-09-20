7e94cd46bad1d963643631cc9750b7ac4b70e0c4
LAKIOTU-STANLEY <lawrence.akiotu-stanley@pau.edu.ng>
2026-09-20 14:25:17 +0100

    Project 1 Week 4

---
+++ week_4/project_1/target/debug/deps/project_1.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Documents\l.akiotu-stanleyCOS101\week_4\project_1\target\debug\deps\project_1.d: src\main.rs
+
+C:\Users\HP\Documents\l.akiotu-stanleyCOS101\week_4\project_1\target\debug\deps\project_1.exe: src\main.rs
+
+src\main.rs:
---
+++ week_4/project_1/target/debug/project_1.d
@@ -0,0 +1,1 @@
+C:\Users\HP\Documents\l.akiotu-stanleyCOS101\week_4\project_1\target\debug\project_1.exe: C:\Users\HP\Documents\l.akiotu-stanleyCOS101\week_4\project_1\src\main.rs
